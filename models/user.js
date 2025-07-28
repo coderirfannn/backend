389a99cb356b33e4de1deb36aafd6d3d6cbb06d8
Mohd Irfan <mohdirfan70097@gmail.com>
2025-07-29 00:07:43 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,50 +1,4 @@
-// const mongoose = require("mongoose");
-// const userSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   password: {
-//     type: String,
-//     required: true,
-//   },
-//   image: {
-//     type: String,
-//     required: true,
-//   },
-//   freindRequests: [
-//     {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "User",
-//     },
-//   ],
-//   friends: [
-//     {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "User",
-//     },
-//   ],
-//   sentFriendRequests: [
-//     {
-//       type: mongoose.Schema.Types.ObjectId,
-//       ref: "User",
-//     },
-//   ],
-// });
-
-
-// const User = mongoose.model("User",userSchema);
-
-// module.exports = User
-
-
 const mongoose = require("mongoose");
-
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -63,7 +17,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  friendRequests: [  // ✅ fixed typo
+  freindRequests: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
@@ -83,7 +37,7 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
-const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+const User = mongoose.model("User",userSchema);
 
+module.exports = User
